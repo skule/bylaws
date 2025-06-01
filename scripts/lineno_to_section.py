579034c044c2795707d01ce1dd5bafeeb9c53aa8
AbyxDev <ken@abyx.dev>
2025-05-31 23:39:06 -0400

    Support arbitrary alpha/roman numbers

--- scripts/lineno_to_section.py
+++ scripts/lineno_to_section.py
@@ -1,12 +1,46 @@
 import re
+from functools import reduce
 
 Section = tuple[int, int, int, int, int]
-ROMAN = [
-    'i', 'ii', 'iii', 'iv', 'v',
-    'vi', 'vii', 'viii', 'ix', 'x',
-    'xi', 'xii', 'xiii', 'xiv', 'xv',
-    'xvi', 'xvii', 'xviii', 'xix', 'xx',
-]
+
+def to_alpha(num: int) -> str:
+    num += 1 # 0-indexed alpha
+    chars = []
+    while num > 0:
+        num, d = divmod(num, 26)
+        if d == 0:
+            num, d = num - 1, d + 26
+        chars.append(chr(ord('a') + d - 1))
+    return ''.join(reversed(chars))
+
+def from_alpha(alpha: str) -> int:
+    alpha = alpha.strip().casefold()
+    if not alpha.isalpha():
+        raise ValueError(alpha)
+    return reduce(lambda r, x: r * 26 + x, (ord(c) - ord('a') + 1 for c in alpha)) - 1
+
+ROMAN_VALUES = 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000
+ROMAN_LETTERS = 'i', 'iv', 'v', 'ix', 'x', 'xl', 'l', 'xc', 'c', 'cd', 'd', 'cm', 'm'
+
+def to_roman(num: int) -> str:
+    num += 1 # 0-indexed alpha
+    letters = []
+    i = len(ROMAN_VALUES) - 1
+    while num > 0:
+        x, num = divmod(num, ROMAN_VALUES[i])
+        letters.extend([ROMAN_LETTERS[i]] * x)
+        i -= 1
+    return ''.join(letters)
+
+def from_roman(roman: str) -> int:
+    i = len(ROMAN_VALUES) - 1
+    num = 0
+    while roman and i >= 0:
+        while roman.startswith(ROMAN_LETTERS[i]):
+            num += ROMAN_VALUES[i]
+            roman = roman[len(ROMAN_LETTERS[i]):]
+        i -= 1
+    return num - 1
 
 def lineno_to_section(lineno: int, lines: list[str]) -> Section:
     if lineno < 0:
@@ -59,7 +93,7 @@ def section_to_str(section: Section) -> str:
         if section[2] >= 0:
             s += '.' + str(section[2] + 1)
             if section[3] >= 0:
-                s += '.' + chr(ord('a') + section[3])
+                s += '.' + to_alpha(section[3])
                 if section[4] >= 0:
-                    s += '.' + ROMAN[section[4]]
+                    s += '.' + to_roman(section[4])
     return s
